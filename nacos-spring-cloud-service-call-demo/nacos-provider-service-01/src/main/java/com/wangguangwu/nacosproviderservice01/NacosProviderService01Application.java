d766c191a49c0f43b93f6d71c38f84e9f8152832
wangguangwu <wangguangwu@pukanghealth.com>
2024-09-02 16:24:54 +0800

    nacos配置服务提供者

---
+++ nacos-spring-cloud-service-call-demo/nacos-provider-service-01/src/main/java/com/wangguangwu/nacosproviderservice01/NacosProviderService01Application.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.nacosproviderservice01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class NacosProviderService01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderService01Application.class, args);
+    }
+
+}
---
+++ nacos-spring-cloud-service-call-demo/nacos-provider-service-02/src/main/java/com/wangguangwu/nacosproviderservice02/NacosProviderService02Application.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.nacosproviderservice02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class NacosProviderService02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderService02Application.class, args);
+    }
+
+}
