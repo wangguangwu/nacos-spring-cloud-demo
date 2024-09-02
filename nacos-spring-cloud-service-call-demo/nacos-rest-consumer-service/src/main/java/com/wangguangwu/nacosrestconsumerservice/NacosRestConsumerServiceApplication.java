506f700ce23b0ffd4c6d99a9646d54e931f76fcb
wangguangwu <wangguangwu@pukanghealth.com>
2024-09-02 16:36:49 +0800

    nacos配置服务消费者-restTemplate

---
+++ nacos-spring-cloud-service-call-demo/nacos-rest-consumer-service/src/main/java/com/wangguangwu/nacosrestconsumerservice/NacosRestConsumerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.nacosrestconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class NacosRestConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosRestConsumerServiceApplication.class, args);
+	}
+
+}
