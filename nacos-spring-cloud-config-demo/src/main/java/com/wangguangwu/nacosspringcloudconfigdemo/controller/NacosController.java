ceaf64667bc87e2bfe2b76f655a5f6962a7c770c
wangguangwu <wangguangwu@pukanghealth.com>
2024-09-02 15:19:45 +0800

    first commit

---
+++ nacos-spring-cloud-config-demo/src/main/java/com/wangguangwu/nacosspringcloudconfigdemo/controller/NacosController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author binghe
+ * @version 1.0.0
+ * @description 从Nacos中获取配置项
+ */
+@RefreshScope
+@RestController
+@Slf4j
+public class NacosController {
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    @Value("${author.name}")
+    private String nacosAuthorName;
+
+    @GetMapping("/nacos/test")
+    public String nacosTest(){
+        String authorName = context.getEnvironment().getProperty("author.name");
+        log.info("获取到的作者姓名为：{}", authorName);
+        return authorName;
+    }
+
+    @GetMapping("/nacos/name")
+    public String nacosName(){
+        log.info("从Nacos中获取到的作者的姓名为：{}", nacosAuthorName);
+        return nacosAuthorName;
+    }
+}
