bc804400a6992cba69be3f2d3838d8c0c36a255d
wangguangwu <wangguangwu@pukanghealth.com>
2024-09-02 16:00:39 +0800

    使用nacos进行服务治理

---
+++ nacos-spring-cloud-discovery-demo/nacos-consumer-demo/src/main/java/com/wangguangwu/nacosconsumerdemo/NacosConsumerDemoApplication.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.nacosconsumerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class NacosConsumerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConsumerDemoApplication.class, args);
+    }
+
+}
---
+++ nacos-spring-cloud-discovery-demo/nacos-consumer-demo/src/main/java/com/wangguangwu/nacosconsumerdemo/controller/ApiController.java
@@ -0,0 +1,26 @@
+package com.wangguangwu.nacosconsumerdemo.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * @author wangguangwu
+ */
+@RestController
+@RequestMapping("api")
+public class ApiController {
+
+    private static final String SERVICE_NAME = "http://nacos-provider-demo";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/callService")
+    public String callService() {
+        return restTemplate.getForObject(SERVICE_NAME + "/api/callService", String.class);
+    }
+}
